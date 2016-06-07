89ba6c55e704ae65bf9abc24d45cd98548cf7720
Yoshio HANAWA <y@hnw.jp>
2016-06-07 19:56:57 +0900

    Initial commit

---
+++ BannerCmd/BannerCmd.h
@@ -0,0 +1,19 @@
+//
+//  BannerCmd.h
+//  BannerCmd
+//
+//  Created by hnw on 2016/06/05.
+//  Copyright © 2016年 hnw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BannerCmd.
+FOUNDATION_EXPORT double BannerCmdVersionNumber;
+
+//! Project version string for BannerCmd.
+FOUNDATION_EXPORT const unsigned char BannerCmdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BannerCmd/PublicHeader.h>
+
+
